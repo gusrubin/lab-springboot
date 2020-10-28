f1db6b4437c3fb3047f32a6caadb370c16389af0
Gustavo Rubin <gustavo.rubin@benfatto.net.br>
2020-10-27 23:42:17 -0300

    hello-world-commandline project

---
+++ hello-world-commandline/src/main/java/com/gusrubin/lab/helloworldcommandline/HelloWorldCommandlineApplication.java
@@ -0,0 +1,13 @@
+package com.gusrubin.lab.helloworldcommandline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldCommandlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldCommandlineApplication.class, args);
+	}
+
+}
---
+++ hello-world-commandline/src/test/java/com/gusrubin/lab/helloworldcommandline/HelloWorldCommandlineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gusrubin.lab.helloworldcommandline;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldCommandlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
